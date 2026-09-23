52bbdb8b33713cda19e57ac6bfc7c8066e7e40e2
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-23 16:43:36 -0300

    parte 2 laço repeticao

---
+++ 3-laco_repeticao/repeticaoesoma.py
@@ -0,0 +1,11 @@
+import os
+os.system('cls')
+
+soma = 0
+for i in range(5):
+    num = int(input('Digite um número inteiro: '))
+    soma = soma + num
+
+print(f'A soma dos 5 numeros é: {soma}')
+
+
